3ce9608a5c6d186cd6cbebab55aa50c3d1a94c22
Alexis QUILLIOT <alexisquilliot95@gmail.com>
2026-02-13 15:57:39 +0100

    fix: réglage probleme port

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import { deckRouter } from './deck/deck.route'
 // Create Express app
 export const app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
